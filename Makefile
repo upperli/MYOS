bc337014874a1428a1a46657bcb7c2c640e6a30e
upperli <18204316366@163.com>
2014-05-18 22:30:00 +0800

    aaa

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 BOOT = ./boot
 MAKE = make
 
-all:A.img
+all:boot A.img
 	dd if=boot/boot.bin of=A.img bs=512 count=1 conv=notrunc
 
 boot:
 	cd $(BOOT) && $(MAKE)
+
+clean:
+	cd $(BOOT) && $(MAKE) clean
--- boot/boot.s
+++ boot/boot.s
@@ -1,29 +1,56 @@
 .code16
 
 
-.text
-BOOTSEG = 0x07c0
-INITSEG = 0x9000
-
 .global _start
 
+.text
+    BOOTSEG = 0x07c0  #注意实际地址=段地址*16+偏移 
+    INITSEG = 0x9000  #将整段代码移动到这个位置
+    SETUPSEG = 0x9020 #SETUP程序位置
+    STACk_SETUP = 0xfef4
+
+#    ljmp    $BOOTSEG,$_start
 _start:
     mov     $BOOTSEG,%ax
     mov     %ax,%ds
     mov     $INITSEG,%ax
     mov     %ax,%es
     mov     $256,%cx
-    sub     %si,%si
-    sub     %di,%di
+    xor     %si,%si
+    xor     %di,%di
     rep
-    movsb
+    movsw
     ljmp    $INITSEG,$go
+.global _start
 go:
     mov     %cs,%ax
     mov     %ax,%ds
     mov     %ax,%es
+
+    mov     $STACk_SETUP,%dx   #将栈地址ss:sp设置为0x9000:fef4,据说要将后面12字节保存一些奇怪的东西
+    mov     %ax,%ss
+    mov     %dx,%sp
+
+
+    push    $0
+    pop     %fs
+    mov     $0x78,%bx
+
+    
+    mov     %fs:(%bx),%si
+    inc     %bx
+    mov     %fs:(%bx),%es
+
+    mov     %dx,%di
+    mov     $6,%cx
+    cld
+    
+    rep
+    movsw
+
     call    DispStr
     jmp     .
+
 DispStr:
     mov     $BootMessage,%ax
     mov     %ax,%bp
